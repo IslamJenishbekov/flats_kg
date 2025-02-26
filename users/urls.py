97672fccd463892f7c75271394ed96965098460d
IslamJenishbekov <islam.akmatov2004@gmail.com>
2025-02-26 10:07:30 +0600

    added logout

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import profile_view, RegisterUser
+from django.contrib.auth.views import LogoutView
 
 app_name = "users"
 
 urlpatterns = [
     path('profile/', profile_view, name='profile'),
     path('register/', RegisterUser.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout')
 ]
